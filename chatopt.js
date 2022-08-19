8f500d4cc06187f1d01c2deb42c7b1c04fcf2ff1
myml <wurongjie1@gmail.com>
2022-08-19 13:48:36 +0800

    Update chatopt.js

--- chatopt.js
+++ chatopt.js
@@ -4,60 +4,63 @@
 // @version      0.1
 // @description  Add Deepin ChatOpt command to github pull request page
 // @author       myml
-// @match        https://github.com/linuxdeepin/*/pull/*
+// @match        https://github.com/linuxdeepin/**
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    // add merge button
-    const mergeBtn = document.createElement("button")
-    mergeBtn.type="button"
-    mergeBtn.textContent="/merge"
-    mergeBtn.classList="btn"
-    mergeBtn.style="float: left;margin-right: 5px;"
-    mergeBtn.addEventListener("click",() => {
-        document.querySelector("#new_comment_field").value="/merge"
-        document.querySelector(".js-new-comment-form").submit()
-        document.querySelector("#new_comment_field").value=""
-    })
-    const actions = document.querySelector("#partial-new-comment-form-actions")
-    const firstChild = actions.firstChild
-    actions.insertBefore(mergeBtn, firstChild)
+    let i = 0
+    const interval = setInterval(()=>{
+        console.log("try append deepin action group")
+        const actions = document.querySelector("#partial-new-comment-form-actions")
+        if(!actions){
+            return
+        }
+        i++
+        if(i>10){
+            clearInterval(interval)
+        }
+        if(document.querySelector(".deepin-action-group")){
+            actions.removeChild(document.querySelector(".deepin-action-group"))
+        }
+        const firstChild = actions.firstChild
+        const group = document.createElement("div")
+        group.classList="deepin-action-group"
+        group.style="float: left;display: flex;flex-wrap: wrap;grid-gap: 0.5rem;max-width: 600px;"
+        actions.insertBefore(group, firstChild)
 
-    // add re-review button
-    const reviewer = document.querySelector(".js-issue-sidebar-form")?.querySelectorAll("span.css-truncate-target") || []
-    Array.from(reviewer).forEach(el => {
-        const username = el.textContent;
-        const reviewBtn = document.createElement("button")
-        reviewBtn.type="button"
-        reviewBtn.textContent="/review @" + username
-        reviewBtn.classList="btn"
-        reviewBtn.style="float: left;"
-        reviewBtn.addEventListener("click",() => {
-            document.querySelector("#new_comment_field").value="/review @" + username
+        // add merge button
+        const mergeBtn = document.createElement("button")
+        mergeBtn.type="button"
+        mergeBtn.textContent="/merge"
+        mergeBtn.classList="btn"
+        mergeBtn.addEventListener("click",() => {
+            document.querySelector("#new_comment_field").value="/merge"
             document.querySelector(".js-new-comment-form").submit()
             document.querySelector("#new_comment_field").value=""
         })
-        actions.insertBefore(reviewBtn, firstChild)
-    })
-    // add re-check button
-    const checks = document.querySelectorAll(".merge-status-item")
-    Array.from(checks).filter(el=>!el.querySelector('.color-fg-success')).map(el=>el.querySelector('.text-emphasized')).forEach(el => {
-        const title = el.getAttribute("title")
-        const name = title.split(" ").slice(-2)[0]
-        const checkBtn = document.createElement("button")
-        checkBtn.type="button"
-        checkBtn.textContent="/check " + name
-        checkBtn.classList="btn"
-        checkBtn.style="float: left;"
-        checkBtn.addEventListener("click",() => {
-            document.querySelector("#new_comment_field").value="/check " + name
-            document.querySelector(".js-new-comment-form").submit()
-            document.querySelector("#new_comment_field").value=""
+        group.appendChild(mergeBtn);
+
+        // add re-check button
+        const checks = document.querySelectorAll(".merge-status-item .color-fg-muted")
+        Array.from(checks).forEach(el => {
+            if(!el.innerText.includes("Failing")){
+               return
+            }
+            const name = el.innerText.slice(0,el.innerText.indexOf("(")).replace(/ /g,'').split('/').slice(1).join('/')
+            const checkBtn = document.createElement("button")
+            checkBtn.type="button"
+            checkBtn.classList="btn"
+            checkBtn.textContent="/check " + name
+            checkBtn.addEventListener("click",() => {
+                document.querySelector("#new_comment_field").value="/check " + name
+                document.querySelector(".js-new-comment-form").submit()
+                document.querySelector("#new_comment_field").value=""
+            })
+            group.appendChild(checkBtn);
         })
-        actions.insertBefore(checkBtn, firstChild)
-    })
+    }, 1000)
     // Your code here...
 })();
